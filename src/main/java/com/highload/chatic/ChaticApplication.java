000228da6c0cdd9248dae62e8b20906020688184
bombanya <proshkin20020129@mail.ru>
2022-09-04 19:36:19 +0300

    init

---
+++ src/main/java/com/highload/chatic/ChaticApplication.java
@@ -0,0 +1,13 @@
+package com.highload.chatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/highload/chatic/ChaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highload.chatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
