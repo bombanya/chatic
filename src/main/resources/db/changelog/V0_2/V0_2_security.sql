e5e73d4029a643d1ea6a12f7e2568e5538ed32c7
Alexey Filimonov <filimonov.alks@gmail.com>
2022-09-07 15:53:59 +0300

    style: reformat sql scripts

--- src/main/resources/db/changelog/V0_2/V0_2_security.sql
+++ src/main/resources/db/changelog/V0_2/V0_2_security.sql
@@ -1,20 +1,23 @@
 --liquibase formatted sql
 
 --changeset bombanya:10
-ALTER TABLE Person ADD COLUMN password varchar(20) NOT NULL;
+ALTER TABLE Person
+    ADD COLUMN password varchar(20) NOT NULL;
 --rollback ALTER TABLE person DROP COLUMN password;
 
 --changeset bombanya:11
-CREATE TABLE AuthRole (
-    id uuid PRIMARY KEY,
+CREATE TABLE AuthRole
+(
+    id   uuid PRIMARY KEY,
     name text NOT NULL UNIQUE
 );
 --rollback DROP TABLE role;
 
 --changeset bombanya:12
-CREATE TABLE PersonRoles (
+CREATE TABLE PersonRoles
+(
     person_id uuid NOT NULL REFERENCES Person ON UPDATE CASCADE ON DELETE CASCADE,
-    role_id uuid NOT NULL REFERENCES AuthRole ON UPDATE CASCADE ON DELETE RESTRICT,
+    role_id   uuid NOT NULL REFERENCES AuthRole ON UPDATE CASCADE ON DELETE RESTRICT,
     PRIMARY KEY (person_id, role_id)
 );
 --rollback DROP TABLE person_roles;
