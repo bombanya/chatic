8a7387c60a8ceaf1d59f4a76920456f64976b5f9
bombanya <proshkin20020129@mail.ru>
2022-09-29 22:45:24 +0300

    feat: basic config for eureka and config-server

---
+++ infrastructure/eureka-server/src/test/java/com/highload/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highload.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
